3cce9bb6b189bec74a5da1f4ddc270ff726dc0a5
Майорова Ульяна <maiorova.uljana07@yandex.ru>
2026-02-21 14:39:42 +0300

    Delete projects_2/task_2_2 directory

--- projects_2/task_2_2/task_2-2_inventory_control.py
+++
@@ -1,3 +0,0 @@
-name_of_the_new_reagent = input('Название нового реактива: ')
-quantity = int(input('Количество(целое число): '))
-print(f'Реактив {name_of_the_new_reagent} поступил на склад в количестве {quantity} шт..')
