02412e51517f7644043f906e25f6dfa1de80e9f3
Майорова Ульяна <maiorova.uljana07@yandex.ru>
2026-03-13 10:03:41 +0300

    Update impulse.sh

--- projects_3/task_3_8/impulse.sh
+++ projects_3/task_3_8/impulse.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "Экспрессия гена $1 составляет $2 единиц."
+read -p "Введите имя гена: " GENE_NAME
+read -p "Введите уровень экспресси (целое число): " EXPRESSION_LEVEL 
+echo "Экспрессия гена $GENE_NAME составляет $EXPRESSION_LEVEL единиц"
